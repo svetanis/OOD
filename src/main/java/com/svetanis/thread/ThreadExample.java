718e882fbee81f4f0b569173e0b9c4ee24da8489
svetanis <svet125@gmail.com>
2024-10-19 13:14:54 -0700

    threads basics

---
+++ src/main/java/com/svetanis/thread/ThreadExample.java
@@ -0,0 +1,22 @@
+package com.svetanis.thread;
+
+public final class ThreadExample extends Thread {
+
+	public int count = 0;
+
+	@Override
+	public void run() {
+		System.out.println("Thread starting.");
+		try {
+			while (count < 5) {
+				Thread.sleep(500);
+				count++;
+				System.out.println("In Thread count=" + count);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Thread interrupted.");
+		}
+		System.out.println("Thread terminating.");
+	}
+
+}
